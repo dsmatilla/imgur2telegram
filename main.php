f99854783bb8685fb1730f8d6dd528db02754fa2
Daniel S. Matilla <daniel@esdis.es>
2017-01-14 16:09:50 +0100

    Create main.php

---
+++ main.php
@@ -0,0 +1,45 @@
+<?php
+    $config['limit'] = 10;
+    $config['tg_bot_token'] = ''; // Put bot token there (99...99:XX...XX format)
+    $config['tg_bot_channel'] = ''; // Channel name to send updates, starting with @
+   
+    $html = fetchUrl("http://imgur.com/hot/time");
+    $urls = array_slice(getUrls($html), 2);
+    $sent = getSent();
+    $to_send = array_slice(array_diff($urls, $sent), 0, $config['limit']);
+    setSent($urls);
+    
+    foreach($to_send as $line) {
+        $link = "http://imgur.com".$line;
+        $url="https://api.telegram.org/bot".$config['tg_bot_token']."/sendMessage?text=".urlencode($link)."&chat_id=".$config['tg_bot_channel'];
+        fetchUrl($url);
+        echo $link."\n";
+    }
+
+function fetchUrl($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $html = curl_exec($ch);
+    curl_close($ch);
+    return $html;
+}
+
+function getUrls($string) {
+    $regex = '/\/gallery\/[^\"\'\/ ]+/i';
+    preg_match_all($regex, $string, $matches);
+    return ($matches[0]);
+}
+
+function getSent() {
+    $sent = array();
+    if(file_exists('sent.txt'))
+        $content = file_get_contents('sent.txt');
+    $sent = explode("\n", $content);
+    return $sent;
+}
+
+function setSent($url_array) {
+    file_put_contents('sent.txt', implode("\n",$url_array));
+}
